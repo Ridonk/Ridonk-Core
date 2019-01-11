b4c9b7ea7b8494cc482795cb6bb5fa85150effef
George Dunbar <georgeddunbar@gmail.com>
2019-01-10 22:49:11 -0500

    Updated docker to new folder structure

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,9 +6,9 @@ docker run \
     --restart always \
     --network=host \
     -e TZ="America/New_York" \
-    -v /home/ridonk/containers/plex/config:/config \
-    -v /home/ridonk/containers/plex/transcode:/transcode \
-    -v /home/ridonk/media:/data \
+    -v /home/ridonk/server/containers/plex/config:/config \
+    -v /home/ridonk/server/containers/plex/transcode:/transcode \
+    -v /home/ridonk/server/media:/data \
     -e PLEX_UID=1000 \
     -e PLEX_GID=1000 \
     plexinc/pms-docker
@@ -19,17 +19,17 @@ docker create \
     -p 8989:8989 \
     -e PUID=1000 -e PGID=1000 \
     -v /etc/localtime:/etc/localtime:ro \
-    -v /home/ridonk/containers/sonarr:/config \
-    -v /home/ridonk/media/tvseries:/tv \
-    -v /home/ridonk/media/downloads:/downloads \
+    -v /home/ridonk/server/containers/sonarr:/config \
+    -v /home/ridonk/server/media/tvseries:/tv \
+    -v /home/ridonk/server/media/downloads:/downloads \
     linuxserver/sonarr
 
 docker create \
     --name=radarr \
     --restart always \
-    -v /home/ridonk/containers/radarr:/config \
-    -v /home/ridonk/media/downloads:/downloads \
-    -v /home/ridonk/media/movies:/movies \
+    -v /home/ridonk/server/containers/radarr:/config \
+    -v /home/ridonk/server/media/downloads:/downloads \
+    -v /home/ridonk/server/media/movies:/movies \
     -v /etc/localtime:/etc/localtime:ro \
     -e PGID=1000 -e PUID=1000  \
     -p 7878:7878 \
@@ -38,9 +38,9 @@ docker create \
 docker create \
     --name=lidarr \
     --restart always \
-    -v /home/ridonk/containers/lidarr:/config \
-    -v /home/ridonk/media/downloads:/downloads \
-    -v /home/ridonk/media/music:/music \
+    -v /home/ridonk/server/containers/lidarr:/config \
+    -v /home/ridonk/server/media/downloads:/downloads \
+    -v /home/ridonk/server/media/music:/music \
     -v /etc/localtime:/etc/localtime:ro \
     -e PGID=1000 -e PUID=1000  \
     -p 8686:8686 \
@@ -48,9 +48,9 @@ docker create \
 
 docker create --name=sabnzbd \
     --restart always \
-    -v /home/ridonk/containers/sabnzbd:/config \
-    -v /home/ridonk/media/downloads:/downloads \
-    -v /home/ridonk/media/downloads/incomplete:/incomplete-downloads \
+    -v /home/ridonk/server/containers/sabnzbd:/config \
+    -v /home/ridonk/server/media/downloads:/downloads \
+    -v /home/ridonk/server/media/downloads/incomplete:/incomplete-downloads \
     -e PGID=1000 -e PUID=1000 \
     -e TZ="America/New_York" \
     -p 8080:8080 -p 9090:9090 \
@@ -61,5 +61,5 @@ docker run -d \
     --name portainer \
     --restart always \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /home/ridonk/containers/portainer:/data \
+    -v /home/ridonk/server/containers/portainer:/data \
     portainer/portainer
